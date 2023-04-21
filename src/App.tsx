af08a7ce6a427b923c93422be0c7da18fe7956cd
OneStepCloser <artinspira@gmail.com>
2023-04-21 20:17:15 +0300

    Добавить шрифт

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
+import {Helmet} from 'react-helmet';
 import logo from './logo.svg';
 
 import 'normalize.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Helmet>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Helmet>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
